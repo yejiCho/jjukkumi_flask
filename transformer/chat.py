30d1eaa8bffa7e1aa0e1e7b34aeb21d4022c300a
choyeji <goe152@naver.com>
2020-06-03 18:06:04 +0900

    [add]chatbot

--- transformer/chat.py
+++ transformer/chat.py
@@ -1,23 +1,23 @@
-from flask import Blueprint, render_template, jsonify, request, redirect, g
+from flask import Blueprint, render_template, jsonify, request, redirect
 from bson import json_util
 import json
 from recom.content_recom import input_movie
-from model import (
-    collection_keyword,collection_movie
-    ,collection_review,collection_user
-    )
-# 해지 추가
-from recom.content_recom import input_movie
+# from transformer.main import predict
 
 chat = Blueprint('chat', __name__, template_folder='templates')
 
 json_test_content =""
+movie_name = ""
 @chat.route('/chat',methods=['GET','POST'])
 def chat_sentence():
     if request.method == 'POST':
         global json_test_content
+        global movie_name
         data = request.get_json()
+        # data = request.form
+        print(data)
         movie_name = data.get('sentence')
+        # print(movie_name)
         movie_code = input_movie(movie_name)
         json_test_code = movie_code.to_json()
         json_test_content = json.loads(json_test_code)
@@ -25,4 +25,5 @@ def chat_sentence():
 
     elif request.method == 'GET':
 
-        return render_template('chat.html',json=json_test_content) 
+        return render_template('chat_test.html',question=movie_name,answer=json_test_content)
+ 
