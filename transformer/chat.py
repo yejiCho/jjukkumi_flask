748e24d8203a2167ab53ee16dba9a7eb2b84fd38
azzoabcz <azzoabczv@gmail.com>
2020-06-05 01:47:10 +0900

    [add]chat

--- transformer/chat.py
+++ transformer/chat.py
@@ -1,26 +1,23 @@
 from flask import Blueprint, render_template, jsonify, request, redirect
 from bson import json_util
 import json
-from recom.content_recom import input_movie
-# from transformer.main import predict
+from transformer.main import predict
 
 chat = Blueprint('chat', __name__, template_folder='templates')
 
-json_test_content =""
-movie_name = ""
+answer =""
+question = ""
 @chat.route('/chat',methods=['GET','POST'])
 def chat_sentence():
     if request.method == 'POST':
-        global json_test_content
-        global movie_name
+        global answer
+        global question
         data = request.get_json()
-        movie_name = data.get('sentence')
-        movie_code = input_movie(movie_name)
-        json_test_code = movie_code.to_json()
-        json_test_content = json.loads(json_test_code)
-        return json_test_content # 덮어씀 json_movie_content
+        question = data.get('sentence')
+        answer = predict(question)
+        return answer # 덮어씀 json_movie_content
 
     elif request.method == 'GET':
 
-        return render_template('chat_test.html',question=movie_name,answer=json_test_content)
+        return render_template('chat_test.html',question=question,answer=answer)
  
