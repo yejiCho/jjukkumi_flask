d46598e7bea37226a75cf0f00ca799a41d0573ae
azzoabcz <azzoabczv@gmail.com>
2020-06-05 11:22:41 +0900

    [add]chatting

--- transformer/chat.py
+++ transformer/chat.py
@@ -5,19 +5,23 @@ from transformer.main import predict
 
 chat = Blueprint('chat', __name__, template_folder='templates')
 
-answer =""
-question = ""
+# answer =""
+# question = ""
+question = []
+answer = []
 @chat.route('/chat',methods=['GET','POST'])
 def chat_sentence():
     if request.method == 'POST':
         global answer
         global question
         data = request.get_json()
-        question = data.get('sentence')
-        answer = predict(question)
+        sentence = data.get('sentence')
+        question.append(sentence)
+        bot_answer = predict(sentence)
+        answer.append(bot_answer)
         return answer # 덮어씀 json_movie_content
 
     elif request.method == 'GET':
-
-        return render_template('chat_test.html',question=question,answer=answer)
+        
+        return render_template('chat_test.html',question=question,answer=answer,zip=zip)
  
